c6d79e545fdddf467b3e07aca10d48c1d278cd60
Simon Chao <ysc406@nyu.edu>
2021-06-23 09:15:32 -0400

    Initial Commit

---
+++ Project7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by YunShou Chao on 6/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
